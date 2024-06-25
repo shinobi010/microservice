7242ba1a4e9a7728726dc01d7771097b784c00cf
OumaimaChebbi-Esprit <chebbi.oumaima@esprit.tn>
2024-06-25 23:36:36 +0100

    Adding Controller package

--- GestionVols/src/main/java/com/AgenceDeVoyage/GestionVols/service/FlightServiceImpl.java
+++ GestionVols/src/main/java/com/AgenceDeVoyage/GestionVols/service/FlightServiceImpl.java
@@ -1,4 +1,39 @@
 package com.AgenceDeVoyage.GestionVols.service;
 
-public class FlightServiceImpl {
+import com.AgenceDeVoyage.GestionVols.entity.Flight;
+import com.AgenceDeVoyage.GestionVols.repository.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FlightServiceImpl implements FlightServiceInterface {
+
+    @Autowired
+    FlightRepository flightRepository;
+    @Override
+    public Flight AddFlight(Flight flight) {
+        return flightRepository.save(flight);
+    }
+
+    @Override
+    public Flight getFlightById(Long identifier) {
+        return flightRepository.findById(identifier).get();
+    }
+
+    @Override
+    public List<Flight> GetAllFlights() {
+        return flightRepository.findAll();
+    }
+
+    @Override
+    public Flight updateFlight(Flight flight) {
+        return flightRepository.save(flight);
+    }
+
+    @Override
+    public void deleteFlight(Long identifier) {
+        flightRepository.deleteById(identifier);
+    }
 }
