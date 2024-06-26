6851430df85375b8cce4e1bd727deb87911830ae
mghrab <mghrab@vermeg.com>
2024-06-26 09:49:16 +0100

    firstt

--- GestionOffre/src/main/java/com/AgenceDeVoyage/GestionOffre/entities/Categorie.java
+++
@@ -1,6 +0,0 @@
-package com.AgenceDeVoyage.GestionOffre.entities;
-
-public enum Categorie {
-	Reveillon,Halloween,VacancesHiver,VacancesPrintemp,VacancesEte
-
-}
--- GestionOffre/src/main/java/com/AgenceDeVoyage/GestionOffre/repositories/PromotionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.AgenceDeVoyage.GestionOffre.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.AgenceDeVoyage.GestionOffre.entities.Promotion;
-
-public interface PromotionRepository extends CrudRepository<Promotion, Long> {
-
-}
