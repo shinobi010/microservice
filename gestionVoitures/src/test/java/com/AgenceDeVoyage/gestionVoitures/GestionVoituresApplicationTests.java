8515f525fb1937b97231d2ec6f96edb2dfa8cb44
Doghri <hadhami.doghri@esprit.tn>
2024-06-26 11:24:04 +0100

    Gestion voiture ressources

---
+++ gestionVoitures/src/test/java/com/AgenceDeVoyage/gestionVoitures/GestionVoituresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgenceDeVoyage.gestionVoitures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVoituresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
